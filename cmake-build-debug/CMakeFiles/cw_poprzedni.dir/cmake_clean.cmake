c5e5e8c23543ffba5f18a744dd44c38a47d73b2b
Empajerek <konradjones@outlook.com>
2024-01-01 00:17:48 +0100

    prev done

---
+++ cmake-build-debug/CMakeFiles/cw_poprzedni.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cw_poprzedni.dir/main.cpp.obj"
+  "CMakeFiles/cw_poprzedni.dir/main.cpp.obj.d"
+  "CMakeFiles/cw_poprzedni.dir/prev.cpp.obj"
+  "CMakeFiles/cw_poprzedni.dir/prev.cpp.obj.d"
+  "cw_poprzedni.exe"
+  "cw_poprzedni.exe.manifest"
+  "cw_poprzedni.pdb"
+  "libcw_poprzedni.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cw_poprzedni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
