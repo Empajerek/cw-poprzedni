f9bdb23ade8f6356d4d1a51f226acbaea1bb6ae7
Konrad <konradjones@outlook.com>
2024-01-02 15:54:45 +0100

    done before testing

--- prev.cpp
+++ prev.cpp
@@ -1,6 +1,5 @@
 #include "prev.h"
 #include <memory>
-#include <algorithm>
 // DEBUG
 #include <iostream>
 #include <string>
@@ -8,15 +7,17 @@
 using namespace std;
 
 #define node_ptr shared_ptr<struct Node>
+#define MAX(x,y) (((x) > (y)) ? (x) : (y))
 
 /**
- * @struct Strukura węzła z których budujemy nasze drzewo przedziałowe
+ * @struct Node
+ * @brief Strukura węzła z których budujemy nasze drzewo przedziałowe
  */
-struct Node {
+static struct Node {
     node_ptr left, right;
     int value;
-    // zwykły konstruktor
-    Node(node_ptr l, node_ptr r, int v) : left(l), right(r), value(v) {}
+    // konstruktor do pustego węzła z wartością
+    Node(int v) : left(nullptr), right(nullptr), value(v) {}
 };
 // Tablica zawierająca korzenie drzew zawierających kolejne indeksy
 static vector<node_ptr> roots;
@@ -29,28 +30,30 @@ static vector<node_ptr> roots;
  * @return średnia
  */
 static int avg(int x, int y){
-    return (x >> 1) + (y >> 1) + (x & 1) + (y & 1);
+    return (x >> 1) + (y >> 1);
 }
 
 /**
  * @brief Przejscie po drzewie do wierzcholka z nr x, i zebranie po drodze wszystkich wezłów
  * @param index drzewo którego indeksu zabieramy
  * @param x szukana wartość w drzewie
- * @return lista węzłów po drodze (bez ostatniego)
+ * @return lista węzłów po drodze (bez pierwszego)
  */
 static vector<node_ptr> getNodesAlongWay(int index, int x){
     vector<node_ptr> gatheredNodes;
     int l = 0, r = 15, current;
     node_ptr curr_Node = roots[index];
-    while(l + 1 != r){
-        gatheredNodes.push_back(curr_Node);
-        if(x >= avg(l, r)){
-            l = avg(l, r);
+    while(l != r){
+        if(x > avg(l, r)){
+            l = avg(l, r) + 1;
             curr_Node = curr_Node->right;
         }else{
             r = avg(l, r);
             curr_Node = curr_Node->left;
         }
+        if(curr_Node == nullptr)
+            break;
+        gatheredNodes.push_back(curr_Node);
     }
     return gatheredNodes;
 }
@@ -58,50 +61,44 @@ static vector<node_ptr> getNodesAlongWay(int index, int x){
 static void printNode(const string prefix, const node_ptr node, bool isLeft){
     if(node != nullptr){
         cout << prefix << (isLeft ? "├──" : "└──" ) << node->value << std::endl;
-        printNode( prefix + (isLeft ? "│   " : "    "), node->left, true);
-        printNode( prefix + (isLeft ? "│   " : "    "), node->right, false);
+        printNode( prefix + (isLeft ? "│   " : "    "), node->right, true);
+        printNode( prefix + (isLeft ? "│   " : "    "), node->left, false);
+    }else{
+        cout << prefix << (isLeft ? "├──" : "└──" ) << "*" << std::endl;
     }
 }
-void printDrzewo(const node_ptr &node){
+static void printDrzewo(const node_ptr &node){
     printNode("", node, false);    
 }
 
 void pushBack(int v){
     int l = 0, p = 15, index = roots.size();
-    bool following;
-    node_ptr last;
+    node_ptr prev_in_tree;
     if(roots.empty()){
-        following = false;
-        last = nullptr;
+        prev_in_tree = nullptr;
     }else{
-        following = true;
-        last = roots.back();
+        prev_in_tree = roots.back();
     }
-    node_ptr previous = make_shared<Node>(nullptr, nullptr, index);
+    node_ptr previous = make_shared<Node>(index);
     roots.push_back(previous);
-    while(l + 1 != p){
-        node_ptr new_node = make_shared<Node>(nullptr, nullptr, index);
-        if(v >= avg(l, p)){
-            l = avg(l, p); 
+    while(l != p){
+        node_ptr new_node = make_shared<Node>(index);
+        if(v > avg(l, p)){
+            l = avg(l, p) + 1; 
             previous->right = new_node;
-            if(following){
-                previous->left = last->left;
-                if(last->right != nullptr)
-                    last = last->right;
-                else following =  false;
-            }else previous->left = nullptr;
-            previous = new_node;
+            if(prev_in_tree != nullptr){
+                previous->left = prev_in_tree->left;
+                prev_in_tree = prev_in_tree->right;
+            }
         } else {
             p = avg(l, p);
             previous->left = new_node;
-            if(following){
-                previous->right = last->left;
-                if(last->left != nullptr)
-                    last = last->left;
-                else following =  false;
-            }else previous->right = nullptr;
-            previous = new_node;
+            if(prev_in_tree != nullptr){
+                previous->right = prev_in_tree->right;
+                prev_in_tree = prev_in_tree->left;
+            }
         }
+        previous = new_node;
     }
 }
 
@@ -109,22 +106,46 @@ void init(const vector<int> &x){
     int x_size = x.size();
     for(int i = 0; i < x_size; i++)
         pushBack(x[i]);
-    printDrzewo(roots.back());
 }
 
 int prevInRange(int index, int lo, int hi){
     int m = -1;
+    int common_node_index = 0;
     vector<node_ptr> lo_nodes = getNodesAlongWay(index, lo);
     vector<node_ptr> hi_nodes = getNodesAlongWay(index, hi);
-    for(int i = lo_nodes.size(); i > 0 && lo_nodes[i] != hi_nodes[i]; i--){
-        if(lo_nodes[i]->right != nullptr)
-            m = (lo_nodes[i]->right->value > m) ? lo_nodes[i]->right->value : m;
-        if(hi_nodes[i]->left != nullptr)
-            m = (lo_nodes[i]->left->value > m) ? lo_nodes[i]->left->value : m;
+    while(common_node_index < lo_nodes.size() && common_node_index < hi_nodes.size() &&
+           lo_nodes[common_node_index] == hi_nodes[common_node_index]) {
+        common_node_index++;
     }
+    // gdy jest co sprawdzać dla wartości mniejszych
+    if(common_node_index != lo_nodes.size()){
+        for(int i = common_node_index; i < lo_nodes.size() - 1; i++) {
+            if (lo_nodes[i]->right != nullptr && lo_nodes[i]->right->value > m 
+                && lo_nodes[i]->right != lo_nodes[i+1]) {
+                    m = lo_nodes[i]->right->value;
+            }
+        }
+        if(lo_nodes.back()->left == nullptr)
+            m = MAX(m, lo_nodes.back()->value);
+    }
+    // gdy jest co sprawdzać dla wartości większych
+    if(common_node_index != hi_nodes.size()){
+        for(int i = common_node_index; i < hi_nodes.size() - 1; i++) {
+            if (hi_nodes[i]->left != nullptr && hi_nodes[i]->left->value > m
+                && hi_nodes[i]->left != hi_nodes[i+1]) {
+                m = hi_nodes[i]->left->value;
+            }
+        }
+        if(hi_nodes.back()->right == nullptr)
+            m = MAX(m, hi_nodes.back()->value);
+    }
+    // gdy obie wartości są takie same, i znaleziono tą wartość
+    if(lo == hi && lo_nodes.back()->left == lo_nodes.back()->right)
+        m = lo_nodes.back()->value;
     return m;
 }
 
 void done(){
     roots.clear();
+    roots.resize(0);
 }
